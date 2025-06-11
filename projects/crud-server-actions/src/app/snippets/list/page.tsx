f1d16451bc756a5fb2fc9f0651ee7ca1a3c5658a
sormiston <sean.david.ormiston@gmail.com>
2025-06-11 13:39:09 +0100

    adds Links and further styles to snippets list page

--- projects/crud-server-actions/src/app/snippets/list/page.tsx
+++ projects/crud-server-actions/src/app/snippets/list/page.tsx
@@ -1,4 +1,5 @@
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function ListSnippetsPage() {
   // This function will fetch all snippets from the database
@@ -7,12 +8,27 @@ export default async function ListSnippetsPage() {
 
   return (
     <>
-      <h3 className="font-bold m-3">Snippets</h3>
-      <ul className="m-3">
+      <div className="flex justify-between items-center">
+        <h3 className="text-3xl font-bold m-3">Snippets</h3>
+        <Link
+          href="/snippets/new"
+          className="flex min-w-16 justify-center border rounded m-3 px-3 py-1 hover:shadow-lg hover:font-semibold focus-visible:shadow-lg focus-visible:font-semibold transition"
+        >
+          New
+        </Link>
+      </div>
+      <ul className="flex flex-col gap-2 m-3">
         {snippets.map((snippet) => (
-          <li key={snippet.id} className="mb-2">
-            <h4 className="font-semibold">{snippet.title}</h4>
-            <pre className="bg-gray-100 p-2 rounded">{snippet.code}</pre>
+          <li key={snippet.id}>
+            <Link
+              href={`/snippets/${snippet.id}`}
+              className="flex justify-between items-center p-2 border rounded group hover:shadow-lg focus-visible:shadow-lg transition"
+            >
+              <h4 className="font-semibold">{snippet.title}</h4>
+              <span className="group-hover:font-semibold transition-all">
+                View
+              </span>
+            </Link>
           </li>
         ))}
       </ul>

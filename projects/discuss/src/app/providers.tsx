6720c7077fd123a2dcea3f68de6b27b8c1e1ec7e
sormiston <sean.david.ormiston@gmail.com>
2025-07-01 19:48:03 +0100

    discuss-app: add nextui + tailwind setup

---
+++ projects/discuss/src/app/providers.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { NextUIProvider } from "@nextui-org/react";
+
+type ProvidersProps = {
+  children: React.ReactNode;
+};
+
+export default function Providers({ children }: ProvidersProps) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}

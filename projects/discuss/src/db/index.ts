b508f35af69fe403f2473db40802e4e8510beb98
sormiston <sean.david.ormiston@gmail.com>
2025-07-02 19:35:56 +0100

    discuss-app: prisma init

--- projects/discuss/src/db/index.ts
+++ projects/discuss/src/db/index.ts
@@ -1,3 +1,8 @@
 import { PrismaClient } from '@/generated/prisma';
 
-export const db = new PrismaClient();
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const db = globalForPrisma.prisma || new PrismaClient();
+// export const db = new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;

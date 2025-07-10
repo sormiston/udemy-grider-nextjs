9f0743450f9f566307e37f9e808553ad7aa6114e
sormiston <sean.david.ormiston@gmail.com>
2025-07-10 16:40:07 +0100

    fix: update button labels for sign in and sign out actions

--- projects/discuss/src/app/page.tsx
+++ projects/discuss/src/app/page.tsx
@@ -10,11 +10,11 @@ export default async function Home() {
     <div>
       {session?.user ? (
         <form action={actions.signOut} className="mr-2 md:mr-0">
-          <Button type="submit">Logout</Button>
+          <Button type="submit">Sign out</Button>
         </form>
       ) : (
         <form action={actions.githubSignIn} className="mr-2 md:mr-0">
-          <Button type="submit">Login</Button>
+          <Button type="submit">Sign in</Button>
         </form>
       )}
       <pre>{JSON.stringify(session, null, 2)}</pre>

25fe1a5f23adf80f4dce3c56c252bb5b1cd24485
sormiston <sean.david.ormiston@gmail.com>
2025-07-22 14:21:15 +0100

    DISCUSS-PKG: refactor: replace native form with NextUI Form component and improve error message formatting

--- projects/discuss/src/components/topics/topic-create-form.tsx
+++ projects/discuss/src/components/topics/topic-create-form.tsx
@@ -7,9 +7,10 @@ import {
   Popover,
   PopoverTrigger,
   PopoverContent,
+  Form,
 } from '@nextui-org/react';
 import type { TopicCreateActionState } from '@/actions/topicCreate';
-import { useActionState, startTransition } from 'react';
+import { useActionState, startTransition, Fragment } from 'react';
 import * as actions from '@/actions';
 
 const initialState: TopicCreateActionState = {
@@ -21,10 +22,7 @@ const initialState: TopicCreateActionState = {
 };
 
 export default function TopicCreateForm() {
-  const [state, dispatch, isPending] = useActionState(
-    actions.topicCreate,
-    initialState
-  );
+  const [state, dispatch] = useActionState(actions.topicCreate, initialState);
 
   const computedFormErrors = state.errors.formErrors.concat(
     state.errors.appStateErrors
@@ -45,20 +43,34 @@ export default function TopicCreateForm() {
     });
   }
 
+  function formatErrorMessage(errors: string[]) {
+    return errors.length > 0
+      ? errors.map((err, index) => (
+          <Fragment key={index}>
+            {`\u2022 ${err}`}
+            <br />
+          </Fragment>
+        ))
+      : undefined;
+  }
+
   return (
     <Popover placement="left-start" onClose={clearActionState}>
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form onSubmit={handleOnSubmit} noValidate>
+        <Form onSubmit={handleOnSubmit}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
             <Input
               name="name"
               label="Name"
               labelPlacement="outside"
-              errorMessage={state.errors.fieldErrors?.name?.join(', ')}
+              errorMessage={
+                state.errors.fieldErrors?.name &&
+                formatErrorMessage(state.errors.fieldErrors.name)
+              }
               isInvalid={!!state.errors.fieldErrors?.name}
               placeholder="your-topic-name"
             />
@@ -67,7 +79,10 @@ export default function TopicCreateForm() {
               name="description"
               label="Description"
               labelPlacement="outside"
-              errorMessage={state.errors.fieldErrors?.description?.join(', ')}
+              errorMessage={
+                state.errors.fieldErrors?.description &&
+                formatErrorMessage(state.errors.fieldErrors.description)
+              }
               isInvalid={!!state.errors.fieldErrors?.description}
               placeholder="Describe your topic"
             />
@@ -78,7 +93,7 @@ export default function TopicCreateForm() {
               </div>
             )}
           </div>
-        </form>
+        </Form>
       </PopoverContent>
     </Popover>
   );
